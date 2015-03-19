0a6ae42cc70211c69902e5e0ce02b4cf8c4ed22a
aulro <robin.aulin@arbetsformedlingen.se>
2015-03-19 13:28:39 +0100

    Fungerande proxykontroll på vis

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var express = require("express");
 var path = require('path');
 var bodyParser = require('body-parser');
 //var multer  = require('multer');
-var ping = require('ping');
+var syncRequest = require('sync-request');
+
+
 
 var app = express();
 
@@ -16,70 +18,99 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 
 /* serves main page */
-app.get("/", function(req, res) {
+app.get("/", function(request, result) {
 	//console.log('static file request : index.html');
-  res.sendFile('index.html', { root: path.join(__dirname, './public') })
+  result.sendFile('index.html', { root: path.join(__dirname, './public') })
 });
 
-app.post("/write", function(req, res) {
+app.post("/write", function(request, result) {
   /* some server side logic */
-  res.send("OK");
+  result.send("OK");
 });
 
-app.post("/pingCheck", function(req,res){
-	res.send("OK");
+app.post("/pingCheck", function(request,result){
+	result.send("OK");
 });
 
-app.get("/pingCheck", function(request,result){
-	var hosts = [{
-		name:'google.com',
-		host:'google.com',
-		sourceUrl:'google.com'
-	},{
-		name:'yahoo.com',
-		host:'yahoo.com',
-		sourceUrl:'yahoo.com'
-	},{
-		name:'facebook.com',
-		host:'facebook.com',
-		sourceUrl:'facebook.com'
-	},{
-		name:'woblin.se',
-		host:'woblin.se',
-		sourceUrl:'woblin.se'
-	},{
-		name:'arbetsformedlingen.se',
-		host:'arbetsformedlingen.se',
-		sourceUrl:'arbetsformedlingen.se'
-	},{
-		name:'sydsvenskan.se',
-		host:'sydsvenskan.se',
-		sourceUrl:'sydsvenskan.se'
-	}];
+
+function updateProxyHostsFile()
+{
+	var urlJsonStr = fs.readFileSync(__dirname+"/url.json", 'utf8');
+	var urlJson = JSON.parse(urlJsonStr);
+
+	urlJson.forEach(function (host) {
+		var res = syncRequest('GET', host.url);
+		var body = res.getBody();
+
+		fs.writeFile(__dirname+"/proxy-"+host.name+".json", body, function(err) {
+	    if(err) {
+	    	console.log(err);
+	      return false;
+	    }
+		});
+
+	});
+	return true;
+}
+
+
+
+app.get("/updateProxyHosts", function(request, result){
+	result.send( updateProxyHostsFile() ? "OK" : "FAIL" );
+});
+
+app.get("/proxyCheck", function(request,result){
+	var hosts = [];
+	var protomatch = /^(https?|ftp):\/\//;
+	var proxyVisBuffer = fs.readFileSync(__dirname+"/proxy-vis.json", 'utf8');
+
+	var hosts = JSON.parse(proxyVisBuffer);
 	
 	var completedPings = [];
 	//Only promise wrapper supports configable ping options
 	hosts.forEach(function (host) {
-	  ping.promise.probe(host.host, {
-	    timeout: 500
-	  }).then(function (res) {
-	    completedPings.push(res);
-	    if(completedPings.length == hosts.length)
-	    {
-	    	result.send(completedPings);
-	    }
-	  });
+		var url = host.targetUrl.replace(protomatch, '');
+
+		var res = null,
+			status = null,
+			success = true;
+		try{
+			res = syncRequest('GET', host.targetUrl);
+			status = res.statusCode;
+		} catch (err) {
+	    console.log(err);
+	    success = false;
+	    status = 999;
+	  }
+
+	  if(status >= 300){
+			console.log(status);
+			success = false;
+		}
+
+		completedPings.push({url:url,status:status,success:success});
 	});
+
+	result.send(completedPings);
 
 });
 
+app.get("/404", function(request,response){
+	response.status(404).send('Not found');
+});
+
+app.get("/500", function(request,response){
+	response.status(500).send('Internal Server Error');
+});
+
+
 /* serves all the static files */
-app.get(/^(.+)$/, function(req, res) {
-  res.sendFile( req.params[0], { root: path.join(__dirname, './public') });
+app.get(/^(.+)$/, function(request, result) {
+  result.sendFile( request.params[0], { root: path.join(__dirname, './public') });
 });
 
 var port = process.env.PORT || 8989;
-var ip = '127.0.0.1';
+var ip = '0.0.0.0';
 
 app.listen(port, ip, function() {
   console.log("Listening on " + port);
--- public/js/controllers/proxystatus.js
+++ public/js/controllers/proxystatus.js
@@ -4,20 +4,52 @@
 
   var app = angular.module('screenModule', ['chart.js']);
 
-  app.controller('proxyStatusCtrl', ['$scope', function ($scope) {
+  app.controller('proxyStatusCtrl', ['$scope', '$http', function ($scope,$http) {
 
 	$scope.init = function(){
-		console.log(Chart.defaults.global.colours);
+		$http.get('/updateProxyHosts').
+		success(function(){
+			updateProxyInfo();
+		}).
+		error(proxyErrorHandling);
+	};
+
+	var updateProxyInfo = function() {
+		$http.get('/proxyCheck').
+			success(proxySuccesshandling).
+			error(proxyErrorHandling);
+	};
+
+	var proxySuccesshandling = function(data, status, headers, config) {
+		console.log(data);
+		var online = 0;
+		var offline = 0;
+		data.forEach(function (host) {
+			console.log(host.status);
+			if(host.status >= 300){
+				offline++;
+			} else {
+				online++;
+			}
+		});
+		$scope.proxyStatusLabels[0] = "Online ("+online+")";
+		$scope.proxyStatusLabels[1] = "Offline ("+offline+")";
+		$scope.proxyStatusData[0] = online;
+		$scope.proxyStatusData[1] = offline;
+	};
+
+	var proxyErrorHandling = function(data, status, headers, config) {
+
 	};
 
 	$scope.proxyStatusSucess = 37;
 	$scope.proxyStatusFail = 2;
 
-	$scope.proxyStatusLabels = ["Online (37)", "Offline (2)"];
-  	$scope.proxyStatusData = [37, 2];
-  	$scope.proxyStatusColors = ['#5cb85c','#d9534f'];
+	$scope.proxyStatusLabels = ["Online","Offline"];
+	$scope.proxyStatusData = [1,0];
+	$scope.proxyStatusColors = ['#5cb85c','#d9534f'];
 
-  	$scope.pieOptions = {animationEasing: "easeOutCirc", animateRotate : false};
+	$scope.pieOptions = {animationEasing: "easeOutCirc", animateRotate : true};
 
 	$scope.init();
 
